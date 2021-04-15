035b2c2afed2389505d2110d2e39eb34eb7dc6a1
Tomonori Ueno <t.uepon@gmail.com>
2021-04-15 16:56:13 +0700

    Complete timer work

--- ClockApp/ViewController.swift
+++ ClockApp/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clockLabel: UILabel!
+    // Prepare Timer
+    var timer: Timer!
+    
+    // At first, how?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // iPhone, what time is it now?
+        let now = Date()
+        // Prepare Date formatter
+        let formatter = DateFormatter()
+        // Define Date format
+        formatter.dateFormat = "dd/MM/yyyy hh:mm:ss"
+        // Let's translate
+        let nowString = formatter.string(from: now)
+        // Get value into label
+        self.clockLabel.text = nowString
+        
+        // Set timer
+        self.timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+        
     }
-
-
+    
+    // When updateClock is called
+    @objc func updateClock() {
+        // iPhone, what time is it now?
+        let now = Date()
+        // Prepare Date formatter
+        let formatter = DateFormatter()
+        // Define Date format
+        formatter.dateFormat = "dd/MM/yyyy hh:mm:ss"
+        // Let's translate
+        let nowString = formatter.string(from: now)
+        // Get value into label
+        self.clockLabel.text = nowString
+    }
 }
 
